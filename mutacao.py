bdcefebf2d45809496e43cd683d02aaac4774273
Rafael Badaró <rafabadaro.15@gmail.com>
2021-05-08 13:06:15 -0300

    Mutacao

--- mutacao.py
+++ mutacao.py
@@ -1,11 +1,30 @@
 
+from random import randint
 
-def cruzar(c1, c2):
-
-
-return novo_cromossomo
+from modelos import *
 
 
-def gerar_mascara():
+def cruzar(c1, c2, atividade, tam_cromossomo):
+
+    mascara = gerar_mascara(tam_cromossomo)
+    pessoas_nova_geracao = []
+
+    for i in range(tam_cromossomo):
+        if mascara[i] == 0:
+            pessoas_nova_geracao.append(c2.pessoas[i])
+        else:
+            pessoas_nova_geracao.append(c1.pessoas[i])
+
+    novo_cromossomo = Cromossomo(pessoas_nova_geracao, atividade)
+
+    return novo_cromossomo
+
+
+def gerar_mascara(tam_cromossomo):
+
+    mascara = []
+
+    for i in range(tam_cromossomo):
+        mascara.append(randint(0, 1))
 
     return mascara
--- roleta.py
+++ roleta.py
@@ -1,19 +1,7 @@
 
-from modelos import Cromossomo
+from modelos import *
 import numpy as np
 
-
-# Pega 2 cromossomos, cruza e add em um vetor de filhos[], fazer isso ate ter o mesmo número de populacao, len(numero_populacao)
-
-# numero_nova_geracao = 0
-# nova_geracao = []
-# while(numero_populacao > numero_nova_geracao)
-# pai1_sorteado = sortear_pai(cromossomos)
-# cromossomos.remove(pai1_sorteado) # - resolve a regra do: Nao pode cruzar o mesmo cromossomo
-# pai2_sorteado = sortear_pai(cromossomos)
-# filho_nova_geracao = cruzarpais(pai1_sorteado, pai2_sorteado)
-# nova_geracao.add(filho_nova_geracao)
-# cromossomos.add(pai1_sorteado)
 
 def preparar_roleta(cromossomos):
     soma_notas_ff = 0
